8395f18d73650f34d4315900c2b85bc878f247df
Harsheet <harsheetpatel@hotmail.com>
2016-04-13 15:20:24 +0100

    Delete review records when restaurant is deleted

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:kfc) { Restaurant.create name: 'KFC' }
+
+  it { is_expected.to belong_to :restaurant }
+
+  describe 'kfc ' do
+    it 'should delete review when kfc is deleted' do
+      review = Review.create(thoughts: 'haha', rating:1)
+      kfc.reviews << review
+      expect{kfc.destroy}.to change{Review.count}
+    end
+  end
 end
