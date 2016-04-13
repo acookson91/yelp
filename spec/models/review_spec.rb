68699b39f593b791e5b1ac9ac56706b7117ed119
Andrew Cookson <acookson91@gmail.com>
2016-04-13 16:46:02 +0100

    Completed version one - added errors

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe Review, type: :model do
       expect{kfc.destroy}.to change{Review.count}
     end
   end
+
+  it "is invalid if the rating is more than 5" do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
+
 end
