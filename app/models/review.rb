cbc17d99288c72964b0a48f2a534c70268e8d9f1
Gabrielle <gabivk@gmail.com>
2016-04-15 16:49:59 +0100

    added endorsement model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   include ReviewsHelper
   belongs_to :restaurant
   belongs_to :user
+  has_many :endorsements
   validates :rating, inclusion: (1..5)
   validates :user_id, :uniqueness => { :scope => :restaurant,
     :message => 'Users may only review a restaurant once'}
